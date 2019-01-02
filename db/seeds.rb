399aa8fe7ab67ae829ae9b751ddecb34be12bf67
pasolre95kim <pasolre95kim@gmail.com>
2019-01-01 20:55:13 -0500

    school tmr

--- db/seeds.rb
+++ db/seeds.rb
@@ -309,18 +309,79 @@ Adoption.create(user_id: 2, animal_id: 5)
 
 #Articles
 
-Article.create(title: "Five Common Misconceptions About Pet Adoption",
+Article.create(title: "Common Misconceptions About Pet Adoption",
   abstract: "We put together a list of common adoption myths, in the hopes that you can gently point friends toward this post when they talk about adding a pet to their family.",
 
   body: "Myth #1: I don’t know what I’m getting
-There may in fact be more information available about an adoptable pet than one from a breeder or pet store.
-Many of the pets posted on Petfinder are in foster care. Foster parents live with their charges 24-7 and can often tell you, in detail, about the pet’s personality and habits. If the pet is at a shelter, the staff or volunteers may be able to tell you what he or she is like.
-At the very least, you can ask the staff if the pet was an owner surrender (rather than a stray) and, if so, what the former owner said about him or her. Quite often pets are given up because the owner faced financial or housing issues (more on that later).
-You can also ask about the health and behavioral evaluations the pet has undergone since arriving at the shelter. In contrast, pet store owners rarely have an idea of what a pet will be like in a home
-Myth #2: I can’t find what I want at a shelter
-If you can’t find the pet you’re looking for on Petfinder, don’t give up.
-Some shelters maintain waiting lists for specific breeds, so don’t be afraid to ask!
-There are also breed-specific rescues for just about every breed, and most of them post their pets on Petfinder.",
+  There may in fact be more information available about an adoptable pet than one from a breeder or pet store.
+  Many of the pets posted on Petfinder are in foster care. Foster parents live with their charges 24-7 and can often tell you, in detail, about the pet’s personality and habits. If the pet is at a shelter, the staff or volunteers may be able to tell you what he or she is like.
+  At the very least, you can ask the staff if the pet was an owner surrender (rather than a stray) and, if so, what the former owner said about him or her. Quite often pets are given up because the owner faced financial or housing issues (more on that later).
+  You can also ask about the health and behavioral evaluations the pet has undergone since arriving at the shelter. In contrast, pet store owners rarely have an idea of what a pet will be like in a home
+  Myth #2: I can’t find what I want at a shelter
+  If you can’t find the pet you’re looking for on Petfinder, don’t give up.
+  Some shelters maintain waiting lists for specific breeds, so don’t be afraid to ask!
+  There are also breed-specific rescues for just about every breed, and most of them post their pets on Petfinder.",
 
   image: "https://d17fnq9dkz9hgj.cloudfront.net/uploads/2012/11/86522359-pet-trust-primer-632x475.jpg"
 )
+
+Article.create(title: "Dog Adoption Checklist",
+  abstract: "Congratulations on deciding to adopt a dog! You are embarking on a wonderful and rewarding relationship.
+  Because adopting a new dog comes with a lot of change for both dog and dog parent,
+  we’ve compiled a checklist to help make the transition as smooth as possible.",
+  body: "Questions for All Adopters:
+
+  Do you have any other dogs and how will they react to a new pet?
+  Is your current residence suited to the dog you’re considering?
+  How will your social life or work obligations affect your ability to care for a dog?
+  Do you have a plan for your new dog during vacations and/or work travel?
+  How do the people you live with feel about having a dog in the house?
+  Are you (or your spouse, partner or roommate) intolerant of hair, dirt and other realities of sharing your home with a dog, such as allergies?
+  Do you or any of your household/family members have health issues that may be affected by a dog?
+  What breed of dog is the best fit with your current lifestyle? (You can find information on specific breeds in our dog breed directory.)
+  Is there tension in the home? Dogs quickly pick up on stress in the home, and it can exacerbate their health and behavior problems.
+  Is there an adult in the family who has agreed to be ultimately responsible for the dog’s care?
+  ",
+ image: "https://images.pexels.com/photos/356378/pexels-photo-356378.jpeg?auto=compress&cs=tinysrgb&h=650&w=940")
+
+ Article.create(title: "Adopting the Right Cat For You",
+  abstract: "The kids have been clamoring for a cat. You’ve held them off for as long as humanly possible,
+  but now you must decide whether or not to make the twenty year commitment to a new feline friend.
+  To dog people, taking on a cat seems like no big deal – no house training, numerous daily walks or obedience classes.
+  But if you are a novice at animal care-taking, hair on the furniture, paw prints on countertops and kitty games at 3 A.M. — not to mention litter box training and daily maintenance — can take some getting used to. Time must be found in hectic schedules for grooming, feeding and interactive play.
+  If you are considering adopting a kitten, factor in plenty of time for socialization and supervision to ensure that the end result will be a well-adjusted adult cat.",
+
+  body: "Searching for Mr. Right
+  Before facing cage after cage of homeless cats, consider your needs and expectations. If yours is a full-time working household, I recommend passing up kittens and adolescents (less than eighteen months old) in favor of a more low-key adult whose energy needs will be easier to meet.
+  If you are a novice cat owner, stay away from “excessive” cats — excessively shy, aggressive or demanding — for they may provide too great a challenge for your first experience.
+  Your best bet is the friendly, outgoing cat, who nudges an outstretched finger offered through the cage bars and who nuzzles and purrs when you hold him in your arms. This profile is a particularly good choice for families with children younger than seven years of age.
+  Is coat color or pattern important? By all means, choose a cat who attracts you,
+  but remember that the gorgeous calico hiding at the back of her cage may well go into prolonged hiding once she is released into your home. A cat who is social and relaxed at a shelter usually has the aplomb to meet the stresses that life throws her way.
+  Consider the whole cat, not just one element.
+  A cat in your life can add warmth, humor and peace of mind. A cat can teach your child empathy for others while keeping her secrets. If you can make the commitment,
+  a cat is waiting to enhance your life in ways only a kindred spirit can.",
+
+  image: "https://images.agoramedia.com/everydayhealth/gcms/Be-Wary-of-Shopping-for-Pet-Meds-Online-1440x810.jpg?width=400"
+)
+
+Article.create(title: "A Rabbit’s Basic Needs",
+  abstract: "The 3 most important foods for a rabbit are hay, hay, and HAY!
+  Contrary to popular belief, rabbits do not need salt licks, vitamins, or hard wooden objects to wear their teeth down.
+  Teeth are kept worn to a proper length by the silicate and lignin content of grass and grass. hays.
+  Do not offer rabbits plants, vegetation, or tree branches unless you are. sure they are not harmful.",
+
+  image: "http://www.sandiegopetsmagazine.com/wp-content/uploads/2018/10/shutterstock_273065675-439x285.jpg",
+  body: "Indoor housing is essential. Domestic rabbits can die from heart attacks at the very approach. of a predator. Other outdoor concerns:
+  fleas (anemia, West Nile virus), ticks, VHD, Baylisacaris procyonis (raccoon roundworm), fly strike
+Intact male and female rabbits must be separated by sex and housed separately. “Accidental” litters occur is shelters with distressing frequency.
+A doe can have a litter every 30 days, and can get pregnant within 30 mins. of giving birth. Immature rabbits can be hard to sex.
+Ask for expert help if you have any doubts.
+Wire flooring can cause and/or exacerbate hock problems, which can become serious enough to require amputation.
+ Use ordinary cat or dog cages, or cover wire flooring with sections of newspaper (can be messy if bunny digs) or plain, brown corrugated cardboard.
+ If dog cages are used, check to be sure the spacing between the cage wires is small enough to prevent a rabbit’s head or limbs from getting stuck.
+Place rabbits away from extreme heat. Avoid placing rabbits on top tier of cages if possible -especially in summer.
+Avoid placing rabbits in the same room with barking dogs if possible. Rabbits usually adapt quickly to barking heard from a distance. Avoid placing rabbits across from (or in full view of) cats.
+Litter-trained rabbits are more easily adopted. Shelter rabbits will usually use litterboxes readily, if the box is place in the corner the rabbit has chosen for urination.
+Line litterbox with section of newspaper and fill with grass hay. Dump daily. Avoid pine and cedar chips as bedding or litter material; aromatic phenols can potentiate liver enzymes and irritate respiratory tract.
+A cardboard “hidey box” placed in the cage can make a rabbit feel more secure. However, this may increase territorial behaviors – particularly in unspayed females.
+Boxes are most useful in wire cages, where the rabbit has no other means of hiding himself.")
